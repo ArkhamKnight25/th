e56400404a5a4b735aede68516a762be3c290c3d
Amrit Amber <bt22csa015@iiitn.ac.in>
2025-06-04 17:17:46 +0530

    Update api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -4,7 +4,9 @@ if (import.meta.env.MODE === "development") {
   API_URL = "http://localhost:5000/api";
 } else {
   // Use your EC2 backend instead of Lambda
+  
   API_URL = "http://15.206.174.62:5000/api";
+  API_RUL="https://xlhsauhi76.execute-api.ap-south-1.amazonaws.com/v1/api"";
 }
 
 export default API_URL;
